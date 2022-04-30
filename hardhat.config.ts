1885c965fdcf4208625d9e3b9b8ffc734ec8f434
AloeSwap <info@aloeswap.com>
2022-04-30 15:05:52 +0200

    Config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,7 +20,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.11',
+    version: '0.8.13',
     //settings: {
     //  optimizer: {
     //    enabled: true,
