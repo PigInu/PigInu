fc5fa4a213e4dfae187ed5eebf404a77f71473b5
AloeSwap <info@aloeswap.com>
2022-05-10 12:29:25 +0200

    spaces

--- contracts/libs/IUniswapV2Factory.sol
+++ contracts/libs/IUniswapV2Factory.sol
@@ -3,28 +3,28 @@
 pragma solidity ^0.8.0;
 
 interface IUniswapV2Factory {
-  event PairCreated(address indexed token0, address indexed token1, address pair, uint);
-  function getPair(address tokenA, address tokenB) external view returns (address pair);
-  function createPair(address tokenA, address tokenB) external returns (address pair);
+ event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+ function getPair(address tokenA, address tokenB) external view returns (address pair);
+ function createPair(address tokenA, address tokenB) external returns (address pair);
 }
 
 contract UniswapV2FactoryMock is IUniswapV2Factory {
-  address private _lpTokenAddress;
-  address private _tokenA;
-  address private _tokenB;
+ address private _lpTokenAddress;
+ address private _tokenA;
+ address private _tokenB;
 
-  constructor(address lpTokenAddress) {
-    _lpTokenAddress = lpTokenAddress;
-  }
+ constructor(address lpTokenAddress) {
+  _lpTokenAddress = lpTokenAddress;
+ }
 
-  function getPair(address tokenA, address tokenB) external view returns (address) {
-    tokenA = tokenB;
-    return _lpTokenAddress;
-  }
+ function getPair(address tokenA, address tokenB) external view returns (address) {
+  tokenA = tokenB;
+  return _lpTokenAddress;
+ }
 
-  function createPair(address tokenA, address tokenB) external returns (address) {
-    _tokenA = tokenA;
-    _tokenB = tokenB;
-    return _lpTokenAddress;
+ function createPair(address tokenA, address tokenB) external returns (address) {
+   _tokenA = tokenA;
+   _tokenB = tokenB;
+   return _lpTokenAddress;
   }
 }
